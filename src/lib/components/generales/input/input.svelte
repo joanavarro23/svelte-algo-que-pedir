f34480e58fd61be92f4c105cd9e796a3b975271b
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-15 16:48:54 -0300

    input comun o textarea

--- src/lib/components/generales/input/input.svelte
+++ src/lib/components/generales/input/input.svelte
@@ -1,9 +1,28 @@
 <script>
   import "./input.css"
 
-  let { for_label, descripcion, type, id, value, cantMax} = $props()
+  let { 
+    nombre = '',
+    type = '',
+    id ='',
+    value = '',
+    placeholder = '',
+    maxlength = null,
+    min=0,
+    required = false, 
+    textarea = false
+    } = $props()
 </script>
+
 <div class="label-input">
-    <label for={for_label}>{descripcion}</label>
-    <input type={type} id={id} value={value} maxlength={cantMax} required>
+    <label for={id}>{nombre}</label>
+    
+    {#if textarea}
+      <textarea class="textarea-descripcion"
+      id={id} {required}
+      >{value}</textarea>
+    {:else}
+      <input 
+      {type} {id} bind:value={value} {placeholder} {min} {maxlength} {required}>
+    {/if}
 </div>
