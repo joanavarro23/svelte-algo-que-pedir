dfe6770ba0d281129322021493f3a9a23b95271f
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-15 11:54:26 -0300

    estilos de menu para linkear a editar-plato

--- src/routes/(app)/menu/+page.svelte
+++ src/routes/(app)/menu/+page.svelte
@@ -1,1 +1,14 @@
-<h1>estoy en menu</h1>
+<script>
+  import './menu.css'
+  import '$lib/components/generales/boton/boton.css'
+</script>
+
+<main class="menu-container main-vista">
+  <header class="titulo-boton">
+      <h1>Gestión del Menú</h1>
+      <button class="boton-primario">
+        Agregar nuevo plato
+        <a href="/editar-plato">editar-plato</a>
+      </button>
+  </header>
+</main>
