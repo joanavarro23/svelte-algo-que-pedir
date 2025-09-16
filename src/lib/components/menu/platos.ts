b2fa7653e315708ac8ab79410ccb05d69c027f97
joanavarro23 <joanavarro23@hotmail.com>
2025-09-15 21:45:26 -0300

    creo clase plato para el menú, con lista de placeholder y creo componente

---
+++ src/lib/components/menu/platos.ts
@@ -0,0 +1,15 @@
+export class Plato {
+  constructor(
+    public titulo: string,
+    public descripcion: string,
+    public imagen: string,
+    public precio: string
+  ) {}
+}
+
+export const platos = [
+  new Plato('Pasta cremosa', 'Deliciosa pasta con salsa cremosa', '/src/lib/assets/pasta-cremosa.png', '12.99'),
+  new Plato('Alitas picantes', 'Alitas de pollo picantes con salsa para mojar', '/src/lib/assets/alitas-picantes.png', '9.99'),
+  new Plato('Ensalada de la Huerta', 'Ensalada fresca con hojas mixtas y vinagreta', '/src/lib/assets/ensalada-huerta.png', '7.50'),
+  new Plato('Hamburguesa con queso', 'Hamburguesa clásica con queso y papas fritas', '/src/lib/assets/hamburguesa-con-queso.png', '10.50'),
+]
