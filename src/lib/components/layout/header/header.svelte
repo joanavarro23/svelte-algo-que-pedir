6fc788391d2459e274191af82db0f18ad953d296
carlasolrocca <carlasolrocca@gmail.com>
2025-09-16 08:32:52 -0300

    agrego container hamburguesa+vista activa

--- src/lib/components/layout/header/header.svelte
+++ src/lib/components/layout/header/header.svelte
@@ -9,25 +9,37 @@
     { href: '/ingrediente', icono: '/ingredientes.svg', label: 'Ingredientes' },
     { href: '/perfil-local', icono: '/usuario.svg', label: 'Cuenta' }
   ]
-
 </script>
 
 <header class="header-pagina">
-  <nav class="barra-navegacion">      <!-- Toda la barra de navegacion del header -->
+  <nav class="barra-navegacion">
+    <!-- Toda la barra de navegacion del header -->
     <a href="/" class="container-logo">
       <img class="logo-app" src={logoUrl} alt="Algo que pedir" />
       <h4>Algo que Pedir</h4>
     </a>
 
-    <ul class="ul-links">             <!-- Lista de enlaces de navegacion -->
+    <ul class="ul-links">
+      <!-- Lista de enlaces de navegacion -->
       {#each urls as { href, icono, label }}
-      <li class= "items-navegacion">
-        <a class="links-navegacion" href={href} aria-label={label}>
-          <img class="iconos-navegacion" src={icono} alt="" aria-hidden="true">
-          <span>{label}</span>
-        </a>
-      </li>
+        <li class="items-navegacion">
+          <a class="links-navegacion" {href} aria-label={label}>
+            <img class="iconos-navegacion" src={icono} alt="" aria-hidden="true" />
+            <span>{label}</span>
+          </a>
+        </li>
       {/each}
     </ul>
+
+    <div class="container-usuario">
+      <button class="hamburguesa">
+        <img class="hamburguesa-icono" src="/hamburguesa-menu.svg" alt="" />
+      </button>
+
+      <a class="vista-activa" href=""></a>
+      <!-- falta function para obtener la vista activa -->>
+
+      <img class="usuario-logo" src="/usuario-chica.svg" alt="Cuenta" />
+    </div>
   </nav>
 </header>
