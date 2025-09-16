6d59a1f65fc64274de49a6351f9d5e8912084845
carlasolrocca <carlasolrocca@gmail.com>
2025-09-16 08:08:13 -0300

    each para barra navegacion header desktop

--- src/lib/components/layout/header/header.svelte
+++ src/lib/components/layout/header/header.svelte
@@ -1,49 +1,33 @@
 <script lang="ts">
   import '$lib/components/layout/header/header.css'
   import logoUrl from '$lib/assets/logo.svg' //esa alternativa o mover todos los svg a static para citar la ruta absoluta como antes
+
+  //Array de todas las urls (c/u un objeto)
+  const urls = [
+    { href: '/pedidos-actuales', icono: '/carrito.svg', label: 'Pedidos' },
+    { href: '/menu', icono: '/menu.svg', label: 'Menu' },
+    { href: '/ingrediente', icono: '/ingredientes.svg', label: 'Ingredientes' },
+    { href: '/perfil-local', icono: '/usuario.svg', label: 'Cuenta' }
+  ]
+
 </script>
 
 <header class="header-pagina">
-  <nav class="barra-navegacion">
-    <!-- Toda la barra de navegacion del header -->
+  <nav class="barra-navegacion">      <!-- Toda la barra de navegacion del header -->
     <a href="/" class="container-logo">
       <img class="logo-app" src={logoUrl} alt="Algo que pedir" />
       <h4>Algo que Pedir</h4>
     </a>
 
-    <ul class="ul-links">
-      <!-- Lista de enlaces de navegacion -->
-      <li class="items-navegacion">
-        <a class="links-navegacion" href="/pedidos-actuales">
-          <!-- <img class="iconos-navegacion" src="../assets/carrito.svg"/> -->
-          <span>Pedidos</span>
+    <ul class="ul-links">             <!-- Lista de enlaces de navegacion -->
+      {#each urls as { href, icono, label }}
+      <li class= "items-navegacion">
+        <a class="links-navegacion" href={href} aria-label={label}>
+          <img class="iconos-navegacion" src={icono} alt="" aria-hidden="true">
+          <span>{label}</span>
         </a>
       </li>
-      <li class="items-navegacion">
-        <a class="links-navegacion" href="/menu">
-          <!-- <img class="iconos-navegacion" src="../assets/menu.svg"/> -->
-          <span>Menu</span>
-        </a>
-      </li>
-      <li class="items-navegacion">
-        <a class="links-navegacion" href="/ingrediente">
-          <!-- <img class="iconos-navegacion" src="../assets/ingredientes.svg"/> -->
-          <span>Ingredientes</span>
-        </a>
-      </li>
-      <li class="items-navegacion">
-        <a class="links-navegacion" href="/perfil-local">
-          <!-- <img class="iconos-navegacion" src="../assets/usuario.svg"/> -->
-          <span>Cuenta</span>
-        </a>
-      </li>
+      {/each}
     </ul>
-
-    <div class="container-usuario">
-      <div class="hamburguesa">
-        <!-- <img class="hamburguesa-icono" src="../assets/hamburguesa-menu.svg"/> -->
-      </div>
-      <!-- <img class="usuario-logo" src="../assets/usuario-chica.svg"/> -->
-    </div>
   </nav>
 </header>
