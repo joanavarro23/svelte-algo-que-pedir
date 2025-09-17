57306003e093b4bbd3612491a7ce33d90b6bc7bc
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-17 11:29:44 -0300

    componente input funcional

--- src/lib/components/generales/input/input.svelte
+++ src/lib/components/generales/input/input.svelte
@@ -1,28 +1,21 @@
-<script>
+<script lang="ts">
   import './input.css'
+  import type { PropsInput } from './propiedades'
 
-  let { 
-    nombre = '',
-    type = '',
-    id ='',
-    value = '',
-    placeholder = '',
-    maxlength = null,
-    min=0,
-    required = false, 
-    textarea = false
-  } = $props()
+  let { nombre_label, type, id, value = '', placeholder = '', required = true, ...rest }: PropsInput = $props()
 </script>
 
 <div class="label-input">
-    <label for={id}>{nombre}</label>
-    
-    {#if textarea}
-      <textarea class="textarea-descripcion"
-      id={id} {required}
-      >{value}</textarea>
-    {:else}
-      <input 
-      {type} {id} bind:value={value} {placeholder} {min} {maxlength} {required}>
-    {/if}
+  <label for={id}>{nombre_label}</label>
+  <input {type} {id} bind:value={value} {placeholder} {...rest} {required}>
 </div>
+
+
+  <!-- {#if textarea}
+    <textarea class="textarea-descripcion"
+    id={id} 
+    {required}
+    {...rest}
+    > bind:value={value} <-- con esto se actualiza automaticamente con la variable padre (la info del back o de la vista menu)
+    </textarea>
+  {:else} -->
---
+++ src/lib/components/generales/input/propiedades.ts
@@ -0,0 +1,12 @@
+import type { HTMLInputAttributes } from 'svelte/elements'
+
+export type InputType = 'checkbox' | 'email' | 'file' | 'number' | 'password' | 'text'
+
+export interface PropsInput extends HTMLInputAttributes {
+  nombre_label: string,
+  type: InputType,
+  id: string,
+  value?: string,
+  placheholder?: string,
+  required?: boolean
+}
