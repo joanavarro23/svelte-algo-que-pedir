b954e543efb442768432a8d07a9f032c4e5e4e67
dampal <damgust@gmail.com>
2025-09-19 00:04:53 -0300

    Convierto login y register a componentes svelte, agrego un componente que oculta el input de password

---
+++ src/lib/components/generales/input/input.svelte
@@ -0,0 +1,25 @@
+<script>
+  import './input.css'
+
+  let {
+    nombre = '',
+    type = '',
+    id = '',
+    value = '',
+    placeholder = '',
+    maxlength = null,
+    min = 0,
+    required = false,
+    textarea = false
+  } = $props()
+</script>
+
+<div class="label-input">
+  <label for={id}>{nombre}</label>
+
+  {#if textarea}
+    <textarea class="textarea-descripcion" {id} {required}>{value}</textarea>
+  {:else}
+    <input {type} {id} bind:value {placeholder} {min} {maxlength} {required} />
+  {/if}
+</div>
