8b8108b02f5d513281036523c6c9f0331d9d762c
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-22 11:16:05 -0300

    fix vista editar plato + integracion de componentes

--- src/routes/(app)/editar-plato/[id]/+page.svelte
+++ src/routes/(app)/editar-plato/[id]/+page.svelte
@@ -1,93 +1,64 @@
 <script>
   import './editar-plato.css'
-  import Boton from '$lib/components/generales/boton/boton.svelte'
-  import Input from '$lib/components/generales/input/input.svelte'
-  import Switch from '$lib/components/generales/switch/switch.svelte'
   import hamburguesa from '$lib/assets/hamburguesa-preview.jpg'
   import { page } from '$app/state'
+  import Input from '$lib/components/generales/input/input.svelte'
+  import Switch from '$lib/components/generales/switch/switch.svelte'
+//   import Boton from '$lib/components/generales/boton/boton.svelte'
  
 
   let nombre = $state('Hamburguesa completa con cheddar')
   let descripcion = $state('Breve descripción de los ingredientes o preparación del plato')
-  let imagen = ''
+  let imagen = $state(hamburguesa)
 </script>
 
-<main class="main-vista">
-  <h1 class="titulo">Editar Plato {page.params.id}</h1>
+<main class="main-vista vista-editar-plato">
+    <h1 class="titulo">Editar Plato {page.params.id}</h1>
 
-  <!-- Descripción del plato -->
-  <section class="contenedor-general editar-plato">
-    <form>
-        <Input 
-      nombre="Nombre del plato*"
-      type="text"
-      id="nombre"
-      value={nombre}
-      maxlength={30}
-      required={true}
-      />
+    <!-- Descripción del plato -->
+    <section class="contenedor-general editar-plato">
+        <form>
+            <Input nombre_label="Nombre del plato*" type="text" id="nombre"
+            bind:value={nombre} maxlength={30} />
 
-      <Input
-      textarea={true}
-      nombre="Descripcion*"
-      id="descripcion"
-      value={descripcion}
-      required={true}
-      />
+            <Input id="descripcion" nombre_label="Descripcion*" textarea={true}
+            bind:value={descripcion} type="" />
 
-      <Input
-      nombre="URL de la imagen del plato*"
-      type="url"
-      id="imagen"
-      value={imagen}
-      placeholder="http://images.google.com/altaburguer.jpg"
-      required={true}
-      />
-    </form>
-    
-    <!-- Imagen de referencia -->
-    <div class="editar-plato__imagen">
-        <img class="foto" src={hamburguesa} alt="Vista previa del plato">
-    </div>
-  </section> 
+            <Input nombre_label="URL de la imagen del plato*" type="file" id="imagen"
+            bind:value={imagen} placeholder="Seleccione una imagen del archivo" />
+        </form>
+        
+        <!-- COMO CONECTO LA IMAGEN !!! ??? -->
+        <!-- Imagen de referencia -->
+        <!-- <div class="editar-plato__imagen">
+            <img class="foto" src={hamburguesa} alt="Vista previa del plato">
+        </div> -->
+    </section> 
 
-  <!-- Costos del plato -->
-  <section class="contenedor-general contenedor-general_especifico">
-    <h2>Costos</h2>
-    <form>
-        <div>
-            <Input
-            nombre="Precio Base*"
-            type="number"
-            id="precio"
-            placeholder="Ej: 500"
-            required={true}
-            />
+    <!-- Costos del plato -->
+    <section class="contenedor-general contenedor-general_especifico">
+        <h2>Costos</h2>
+        <form>
+            <Input nombre_label="Precio Base*" type="number" id="precio" placeholder="Ej: 500" />
+            <div>
+                <Switch id="platoDeAutor" titulo="Plato de Autor" 
+                subtitulo="Aplica un porcentaje adicional al precio de venta" />
+            </div>
+            <div>
+                <Switch id="platoDePromocion" titulo="Plato en Promoción"
+                subtitulo="Aplica un descuento al precio de venta" />
+            </div> 
+        </form>
+    </section>
+
+    <!-- Detalle de los ingredientes --> 
+    <section class="contenedor-general contenedor-general_especifico">
+        <h2>Ingredientes</h2>
+        <div class="contenedor_titulo-span">
+            <h3 class="subtitulo">Costo de producción</h3><span>$ 100</span>
         </div>
-        <div>
-            <Switch
-            id="platoDeAutor"
-            titulo="Plato de Autor"
-            subtitulo="Aplica un porcentaje adicional al precio de venta"
-            />
-        </div>
-        <div>
-            <Switch
-            id="platoDePromocion"
-            titulo="Plato en Promoción"
-            subtitulo="Aplica un descuento al precio de venta"
-            />
-        </div> 
-    </form>
-</section> 
+    </section>
 
-<!-- Detalle de los ingredientes -->
-<section class="contenedor-general contenedor-general_especifico">
-    <h2>Ingredientes</h2>
-    <div class="contenedor_titulo-span">
-        <h3 class="subtitulo">Costo de producción</h3><span>$ 100</span>
-    </div>
-</section>
   <!-- TABLA CONFIGURAR COMO COMPONENTE
       <article class="contenedor-tabla">
           <table>
@@ -146,8 +117,8 @@
  -->
 
   <!-- Botones finales -->
-  <div class="botones-juntos">
+  <!-- <div class="botones-juntos">
     <Boton nombre="Guardar cambios"/>
     <Boton tipo='secundario' nombre="Descartar cambios"/>
-  </div>
+  </div> -->
 </main>
