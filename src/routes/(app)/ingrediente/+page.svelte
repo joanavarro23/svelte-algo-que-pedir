57848263539250f82cb3954dd88f75ce1a9297be
joanavarro23 <joanavarro23@hotmail.com>
2025-09-22 17:47:49 -0300

    acomodo estilado de vista ingredientes

--- src/routes/(app)/ingrediente/+page.svelte
+++ src/routes/(app)/ingrediente/+page.svelte
@@ -1,18 +1,19 @@
 <script lang='ts'>
 
-  import '$lib/styles/pages/tablas.css'
+  import '$lib/styles/tablas.css'
+  import '$lib/components/ingredientes/ingredientes.css'
   import Boton from '$lib/components/generales/boton/boton.svelte'
   import IngredienteRow from '$lib/components/ingredientes/IngredienteRow.svelte'
   import { ingredientes } from '$lib/components/ingredientes/ingredientes'
 
 </script>
 
-<main class="ing rediente-container main-vista">
-        <header class="titulo-boton">
+<main class="ingrediente-container main-vista">
+        <header class="boton-titulo">
             <h1>Ingredientes</h1>
             <Boton onclick={()=>{
           
-            }} nombre= "Agregar nuevo plato"/>
+            }} nombre= "Nuevo Ingrediente"/>
         </header>
         <section class="contenedor-tabla"> <!-- Sin este section no me pone los bordes -->
             <table>
