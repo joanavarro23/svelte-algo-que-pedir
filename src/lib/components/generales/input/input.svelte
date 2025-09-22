f88d33d7c9c299f85d0bd18ea089d1e5319ba55c
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-22 11:15:09 -0300

    mejoras para el input

--- src/lib/components/generales/input/input.svelte
+++ src/lib/components/generales/input/input.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
   import './input.css'
-  import type { PropsInput } from './propiedades'
+  import type { HTMLInputAttributes } from 'svelte/elements'
 
-  let { nombre_label, type, id, value = '', required = true, ...rest }: PropsInput = $props()
+  // el tipo '' es para el textarea que se trabaja sin type, pero
+  //   lo quiero controlar desde este componente para mayor control de todos los inputs distintos.  
+  export type InputType = '' | 'checkbox' | 'email' | 'file' | 'number' | 'password' | 'text'
+
+  interface PropsInput extends HTMLInputAttributes {
+    nombre_label: string,
+    type: InputType,
+    id: string,
+    value?: string,
+    textarea?: boolean
+  }
+  
+  let { nombre_label, type, id, value = $bindable(''), required = true, textarea = false, ...rest }: PropsInput = $props()
 </script>
 
-<div class="label-input">
-  <label for={id}>{nombre_label}</label>
-  <input {type} {id} bind:value={value} {...rest} {required}>
-</div>
+{#if !textarea}
+  <div class="label-input">
+    <label for={id}>{nombre_label}</label>
+    <input {type} {id} bind:value={value} {...rest} {required}>
+  </div>
+{:else}
+  <div class="label-input">
+    <label for={id}>{nombre_label}</label>
+    <textarea class="textarea-descripcion" id={id} bind:value={value} {required}></textarea>
+  </div>
+{/if}
