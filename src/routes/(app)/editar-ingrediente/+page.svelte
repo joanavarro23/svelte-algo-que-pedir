f236569a48f8aa5b127b61e1896702711f1a63fb
carlasolrocca <carlasolrocca@gmail.com>
2025-09-22 23:11:24 -0300

    Agrego css y primeros inputs de editar-ingrediente

--- src/routes/(app)/editar-ingrediente/+page.svelte
+++ src/routes/(app)/editar-ingrediente/+page.svelte
@@ -1,1 +1,31 @@
-<h1>estas en editar ingredientes</h1>
+<script>
+  import './editar-ingrediente.css'
+  import Input from '$lib/components/generales/input/input.svelte'
+</script>
+
+<main class="vista-edicion-ingrediente main-vista">
+  <!-- Contenedor de toda la vista -->
+  <h1 class="titulo-edicion">Editar ingrediente</h1>
+  <section class="container-edicion contenedor-general">
+    <!-- Contenedor de los campos de edicion ingrediente -->
+    <article class="item-input-edicion">
+      <form>
+        <Input
+          nombre="Nombre del ingrediente*"
+          type="text"
+          id="nombre-ingrediente"
+          placeholder="Ingresa el nombre del ingrediente..."
+          required={true}
+        />
+
+        <Input
+          nombre="Costo*"
+          type="number"
+          id="costo-ingrediente"
+          placeholder="Ingresa su costo..."
+          required={true}
+        /> 
+      </form>
+    </article>
+  </section>
+</main>
