f912d52c03417dc019e80ab913d96f09cb11af9f
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-23 20:04:22 -0300

    boton sin snippet

--- src/lib/components/generales/boton/boton.svelte
+++ src/lib/components/generales/boton/boton.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import './boton.css'
   import type { HTMLButtonAttributes } from 'svelte/elements'
-  import type { Snippet } from 'svelte'
   
   interface PropsButton extends HTMLButtonAttributes {
-    tipo?: 'icono' | 'primario' | 'secundario'
-    nombre: Snippet
+    tipo?: 'primario' | 'secundario'
+    nombre: string 
   }
   const { tipo = 'primario', nombre, ...rest }: PropsButton = $props()
 </script>
 
 <button class={`boton-${tipo}`} {...rest}>
-  {@render nombre()}
+  {nombre}
 </button>
