81aeb6192efc08126be8885e3f38c27814f78c64
carlasolrocca <carlasolrocca@gmail.com>
2025-09-28 12:14:24 -0300

    creo archivo pedido-card

---
+++ src/lib/components/pedidos/pedido-card.svelte
@@ -0,0 +1,5 @@
+<script>
+  import type { Pedido } from "$lib/types/pedido.ts"
+  import type { MedioDePago } from "$lib/types/pedido.ts";
+  import type { EstadoDelPedido } from "$lib/types/pedido.ts"
+</script>
