3defd3a5e7b2a33cc3ed9eda61df44ead39e9a87
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-29 19:38:26 -0300

    modifico ruta del mock plato y boton

--- src/routes/(app)/menu/+page.svelte
+++ src/routes/(app)/menu/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import Boton from '$lib/components/generales/boton/boton.svelte'
   import ItemMenu from '$lib/components/menu/ItemMenu.svelte'
-  import { platos } from '$lib/components/menu/platos'
+  import { platosMock } from '$lib/data/mock/PLATO'
 </script>
 
 <main class="menu-container main-vista">
@@ -10,13 +10,13 @@
       <a href="./editar-plato">
         <Boton onclick={()=>{
           
-        }} nombre="Agregar nuevo plato" />
+        }}>Agregar nuevo plato</Boton>
       </a>
   </header>
   <section class="contenedor-general">
       <h2>Platos Disponibles</h2>
       <ul class="lista-menu">
-        {#each platos as plato (plato.id)}
+        {#each platosMock as plato (plato.id)}
           <a href="./editar-plato/{plato.id}">
             <ItemMenu {plato} />
           </a>
