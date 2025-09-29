1b33960850a67dbc7c2bc31faac49ab2f6b16747
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-29 19:38:56 -0300

    fix editar plato

--- src/routes/(app)/editar-plato/[id]/+page.svelte
+++ src/routes/(app)/editar-plato/[id]/+page.svelte
@@ -1,55 +1,45 @@
-<script>
+<script lang="ts">
   import './editar-plato.css'
-  import { page } from '$app/state'
   
   // Componentes
   import Input from '$lib/components/generales/input/input.svelte'
+  import Textarea from '$lib/components/generales/input/textarea.svelte'
   import Switch from '$lib/components/generales/switch/switch.svelte'
   import Boton from '$lib/components/generales/boton/boton.svelte'
   import Tabla from '$lib/components/generales/tabla/Tabla.svelte'
+  import Validador from '$lib/utils/validador.svelte'
   
   // Lista de ingredientes
   import { ingredientes } from '$lib/components/ingredientes/ingredientes'
   import IngredienteRow from '$lib/components/ingredientes/IngredienteRow.svelte'
+  // Platos
+  import { PlatoForm } from '$lib/data/forms/platoForm.svelte'
   
-  import hamburguesa from '$lib/assets/hamburguesa-preview.jpg'
-  let nombre = 'Hamburguesa completa con cheddar'
-  let descripcion = 'Breve descripción de los ingredientes o preparación del plato'
+  let plato: PlatoForm = new PlatoForm()
 </script>
 
-{#snippet nombreColumnas()}
-<th>Nombre</th>
-<th>Grupo</th>
-<th class="icono">Origen</th>
-<th class="icono">Acciones</th>
-{/snippet}
-{#snippet datosFilas()}
-{#each ingredientes as ingrediente (ingrediente.id)}
-<IngredienteRow {ingrediente} editarPlato={true} />
-{/each}
-{/snippet}
-
-
 <main class="main-vista vista-editar-plato">
-    <h1 class="titulo">Editar Plato {page.params.id}</h1>
+    <h1 class="titulo">Editar Plato</h1>
 
     <!-- Descripción del plato -->
     <section class="contenedor-general editar-plato">
         <form>
-            <Input nombre_label="Nombre del plato*" type="text" id="nombre"
-            bind:value={nombre} maxlength={30} placeholder="Ej: Hamburguesa completa con cheddar"/>
+            <Input data-testid="titulo" nombre_label="Nombre del plato*" type="text" id="nombre"
+            bind:value={plato.titulo} maxlength={30} placeholder="Ej: Hamburguesa completa con cheddar"/>
+            <Validador elemento={plato} atributo="titulo"/>
 
-            <Input id="descripcion" nombre_label="Descripcion*" textarea={true}
-            bind:value={descripcion} type=""/>
+            <Textarea data-testid="descripcion" id="descripcion" nombre_label="Descripcion*" textarea={true}
+            bind:value={plato.descripcion}/>
+            <Validador elemento={plato} atributo="descripcion"/>
 
-            <!-- AGREGAR VALUE=PLATO.IMAGEN -->
-            <Input nombre_label="URL de la imagen del plato*" type="file" id="imagen"
-            accept="image/jpeg,image/jpg,image/png" />
+            <Input data-testid="imagen" nombre_label="URL de la imagen del plato*" type="file" id="imagen"
+            accept="image/jpeg,image/jpg,image/png" bind:value={plato.imagen}/>
+            <Validador elemento={plato} atributo="imagen"/>
         </form>
         
         <!-- Imagen de referencia -->
         <div class="editar-plato__imagen">
-            <img class="foto" src={hamburguesa} alt="Vista previa del plato">
+            <img class="foto" src={plato.imagen} alt="Vista previa del plato">
         </div>
     </section> 
 
@@ -57,11 +47,11 @@
     <section class="contenedor-general contenedor-general_especifico">
         <h2>Costos</h2>
         <form class="costos-plato">
-            <Input nombre_label="Precio Base*" type="number" id="precio" placeholder="Ej: 500" min=0 />
-            <Switch id="platoDeAutor" titulo="Plato de Autor" 
-            subtitulo="Aplica un porcentaje adicional al precio de venta" />
-            <Switch id="platoDePromocion" titulo="Plato en Promoción"
-            subtitulo="Aplica un descuento al precio de venta" />
+            <Input data-testid="precio" nombre_label="Precio Base*" type="number" id="precio" placeholder="Ej: 500" min=0 />
+            <Validador elemento={plato} atributo="precio"/>
+
+            <Switch id="platoDeAutor" titulo="Plato de Autor" subtitulo="Aplica un porcentaje adicional al precio de venta" />
+            <Switch id="platoDePromocion" titulo="Plato en Promoción" subtitulo="Aplica un descuento al precio de venta" />
         </form>
     </section>
 
@@ -72,11 +62,23 @@
             <h3 class="subtitulo">Costo de producción</h3><span>$ 100</span>
         </div>
 
-        <Tabla {nombreColumnas} {datosFilas} />
+        <Tabla>
+            {#snippet nombreColumnas()}
+                <th>Nombre</th>
+                <th>Grupo</th>
+                <th class="icono">Origen</th>
+                <th class="icono">Acciones</th>
+            {/snippet}
+            {#snippet datosFilas()}
+                {#each ingredientes as ingrediente (ingrediente.id)}
+                    <IngredienteRow {ingrediente} editarPlato={true} />
+                {/each}
+            {/snippet}
+        </Tabla>
     </section>
 
     <div class="botones-juntos">
-        <Boton nombre="Guardar cambios"/>
-        <Boton tipo='secundario' nombre="Descartar cambios"/>
+        <Boton data-testid="btnGuardar" type="submit" onclick={() => plato.guardar()}>Guardar cambios</Boton>
+        <Boton data-testid="btnDescartar" tipo='secundario'>Descartar cambios</Boton>
     </div>
 </main>
