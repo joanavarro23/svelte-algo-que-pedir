277c09843f8402f58e0fa68cd250efb10587c6e6
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-29 19:39:54 -0300

    muevo model plato

--- src/lib/data/models/plato.ts
+++ src/lib/data/models/plato.ts
@@ -1,10 +1,11 @@
+// Clase para guardar los datos puros del Plato. Es parte del Modelo de dominio (back)
 export class Plato {
   constructor(
-    public id: number,
+    public id: number | undefined,
     public titulo: string,
     public descripcion: string,
-    public imagen: File,
-    public precio: number,
+    public imagen: string | null,    // DEBERIA SER FILE?
+    public precio: number | undefined,
     public platoDeAutor: boolean,
     public platoDePromocion: boolean
   ) {}
