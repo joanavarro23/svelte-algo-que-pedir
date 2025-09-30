a90f2c03ad291b6ad45325c4b1bf7eb6091a1648
joanavarro23 <joanavarro23@hotmail.com>
2025-09-29 23:19:37 -0300

    ajusto menu para que use el mock

--- src/routes/(app)/menu/+page.svelte
+++ src/routes/(app)/menu/+page.svelte
@@ -2,7 +2,7 @@
   import { goto } from '$app/navigation'
   import Boton from '$lib/components/generales/boton/boton.svelte'
   import ItemMenu from '$lib/components/menu/ItemMenu.svelte'
-  import { platos } from '$lib/components/menu/platos'
+  import { PLATOS_MOCK } from '$lib/data/mocks/platosMock'
 </script>
 
 <main class="menu-container main-vista">
@@ -15,7 +15,7 @@
   <section class="contenedor-general">
       <h2>Platos Disponibles</h2>
       <ul class="lista-menu">
-        {#each platos as plato (plato.id)}
+        {#each PLATOS_MOCK as plato (plato.id)}
           <a href="./editar-plato/{plato.id}">
             <ItemMenu {plato} />
           </a>
