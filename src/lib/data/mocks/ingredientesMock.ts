0e60e43602972ac63b87244e68bd0d324c22fe5e
joanavarro23 <joanavarro23@hotmail.com>
2025-09-29 23:10:19 -0300

    mock ingredientes

---
+++ src/lib/data/mocks/ingredientesMock.ts
@@ -0,0 +1,40 @@
+import type { Ingrediente } from '$lib/types'
+import { GrupoAlimenticio } from '$lib/types'
+
+export const INGREDIENTES_MOCK: Ingrediente[] = [
+  {
+    id: 1,
+    nombre: 'Tomate',
+    costo: '0.50',
+    grupo: GrupoAlimenticio.FRUTAS_Y_VERDURAS,
+    origen: 'vegetal'
+  },
+  {
+    id: 2,
+    nombre: 'Pechuga de pollo',
+    costo: '3.00',
+    grupo: GrupoAlimenticio.PROTEINAS,
+    origen: 'animal'
+  },
+  {
+    id: 3,
+    nombre: 'Arroz',
+    costo: '1.00',
+    grupo: GrupoAlimenticio.CEREALES_Y_TUBERCULOS,
+    origen: 'vegetal'
+  },
+  {
+    id: 4,
+    nombre: 'Leche',
+    costo: '2.00',
+    grupo: GrupoAlimenticio.LACTEOS,
+    origen: 'animal'
+  },
+  {
+    id: 5,
+    nombre: 'Palta',
+    costo: '1.50',
+    grupo: GrupoAlimenticio.FRUTAS_Y_VERDURAS,
+    origen: 'vegetal'
+  }
+]
