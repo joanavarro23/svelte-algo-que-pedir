88d6a6e47c87aa846100c6fc70997317863de856
joanavarro23 <joanavarro23@hotmail.com>
2025-09-29 23:02:22 -0300

    type de ingrediente

---
+++ src/lib/types/ingrediente.ts
@@ -0,0 +1,17 @@
+export type Ingrediente = {
+  nombre: string,
+  costo: string,
+  grupo: GrupoAlimenticio,
+  origen: Origen
+}
+
+export type Origen = 'animal' | 'vegetal'
+
+export enum GrupoAlimenticio {
+  CEREALES_Y_TUBERCULOS = 'Cereales y tubérculos',
+  AZUCARES_Y_DULCES = 'Azúcares y dulces',
+  LACTEOS = 'Lácteos',
+  FRUTAS_Y_VERDURAS = 'Frutas y verduras',
+  GRASAS_Y_ACEITES = 'Grasas y aceites',
+  PROTEINAS = 'Proteínas'
+}
