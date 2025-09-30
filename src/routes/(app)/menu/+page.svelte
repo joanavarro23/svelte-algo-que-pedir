a82477028dc3ec7adf3fe717b35b5c214b9f8052
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-30 08:47:12 -0300

    se cambio la variable para los platos mock en menu

--- src/routes/(app)/menu/+page.svelte
+++ src/routes/(app)/menu/+page.svelte
@@ -1,26 +1,24 @@
 <script>
   import Boton from '$lib/components/generales/boton/boton.svelte'
   import ItemMenu from '$lib/components/menu/ItemMenu.svelte'
-  import { platosMock } from '$lib/data/mock/PLATO'
+  import { PLATOS_MOCK } from '$lib/data/mock/PLATO'
 </script>
 
 <main class="menu-container main-vista">
   <header class="boton-titulo">
-      <h1>Gestión del Menú</h1>
-      <a href="./editar-plato">
-        <Boton onclick={()=>{
-          
-        }}>Agregar nuevo plato</Boton>
-      </a>
+    <h1>Gestión del Menú</h1>
+    <a href="./editar-plato">
+      <Boton onclick={() => {}}>Agregar nuevo plato</Boton>
+    </a>
   </header>
   <section class="contenedor-general">
-      <h2>Platos Disponibles</h2>
-      <ul class="lista-menu">
-        {#each platosMock as plato (plato.id)}
-          <a href="./editar-plato/{plato.id}">
-            <ItemMenu {plato} />
-          </a>
-        {/each}
-      </ul>
+    <h2>Platos Disponibles</h2>
+    <ul class="lista-menu">
+      {#each PLATOS_MOCK as plato (plato.id)}
+        <a href="./editar-plato/{plato.id}">
+          <ItemMenu {plato} />
+        </a>
+      {/each}
+    </ul>
   </section>
 </main>
