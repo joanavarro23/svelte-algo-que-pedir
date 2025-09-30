b381f88c6961f91d438a0c06beddfe641abfaae6
joanavarro23 <joanavarro23@hotmail.com>
2025-09-29 21:54:40 -0300

    cambios de tabulación y comillas por warnings del linter

--- src/routes/(app)/perfil-local/+page.svelte
+++ src/routes/(app)/perfil-local/+page.svelte
@@ -5,20 +5,20 @@
   import ProfileCard from '$lib/components/perfil-local/profile-card.svelte'
 
   const mockInfoLocal = {
-      nombreLocal: "Taberna de Moe",
-      urlImagen: "https://www.clarin.com/img/2017/10/05/SkWTevV3-_1200x0.jpg",
-      direccion: "Walnut Street",
-      altura: "123",
-      latitud: "39.808327",
-      longitud: "-89.643204",
-      porcentajeApp: "3",
-      porcentajeAutor: "3",
-      metodosDePago: {
-        QR: true,
-        Efectivo: true,
-        Transferencia: false
-      }
+    nombreLocal: 'Taberna de Moe',
+    urlImagen: 'https://www.clarin.com/img/2017/10/05/SkWTevV3-_1200x0.jpg',
+    direccion: 'Walnut Street',
+    altura: '123',
+    latitud: '39.808327',
+    longitud: '-89.643204',
+    porcentajeApp: '3',
+    porcentajeAutor: '3',
+    metodosDePago: {
+      QR: true,
+      Efectivo: true,
+      Transferencia: false
     }
+  }
   function guardarCambios() {
     /* Muesta los datos ingresados; después será la acción que va a enviar los datos del form al back*/
     const datosLocal = {
