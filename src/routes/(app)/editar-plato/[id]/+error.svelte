9eb0f4f4d384febaeffaaf4ed453233985c00e64
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-30 08:47:24 -0300

    agrego error para id no encontrado

--- src/routes/(app)/editar-plato/[id]/+error.svelte
+++ src/routes/(app)/editar-plato/[id]/+error.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
   import { page } from '$app/state'
+  import errorImg  from 'lib/assets/errorImg.png'
 </script>
 
 <h1>Error: {page.status}</h1>
-<p>{page.error?.message}</p>
+<p>{page.error?.message ?? 'Error desconocido, contactese con el administrador de la página.'}</p>
+<img src={errorImg} alt="Imagen de error">
+
+<style>
+  h1, p, img {
+    align-items: center;
+  }
+  p {
+    color: red;
+  }
+  img {
+    max-width: 300px;
+  }
+</style>
