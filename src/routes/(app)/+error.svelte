7d2ccb698070563bb74cf65c04ef6c3368b52670
Sol Alcaraz <alcarazmsol@gmail.com>
2025-10-03 12:51:22 -0300

    error centrado

--- src/routes/(app)/+error.svelte
+++ src/routes/(app)/+error.svelte
@@ -2,19 +2,23 @@
   import { page } from '$app/state'
   import errorImg  from '$lib/assets/errorImg.png'
 </script>
-
-<h1>Error: {page.status}</h1>
-<p>{page.error?.message ?? 'Error desconocido, contactese con el administrador de la página.'}</p>
-<img src={errorImg} alt="Imagen de error">
+<div class="error-contenedor">
+  <h1>Error: {page.status}</h1>
+  <p>{page.error?.message ?? 'Error desconocido, contactese con el administrador de la página.'}</p>
+  <img src={errorImg} alt="Imagen de error">
+</div>
 
 <style>
-  h1, p, img {
+  .error-contenedor {
+    display: flex;
+    flex-direction: column;
     align-items: center;
+    margin-top: 1rem;
   }
   p {
     color: red;
   }
   img {
-    max-width: 300px;
+    max-width: 600px;
   }
 </style>
