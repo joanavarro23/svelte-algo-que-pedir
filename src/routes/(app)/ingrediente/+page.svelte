743a6cd672a2b1e8d65a94c98c2608ba967e5a01
Sol Alcaraz <alcarazmsol@gmail.com>
2025-10-04 11:42:32 -0300

    implemente iconos accion + estilos q pasan a ese vista

--- src/routes/(app)/ingrediente/+page.svelte
+++ src/routes/(app)/ingrediente/+page.svelte
@@ -1,14 +1,16 @@
 <script lang='ts'>
   import Boton from '$lib/components/generales/boton/boton.svelte'
+  import IconoBoton from '$lib/components/generales/icono boton/iconoBoton.svelte'
   import IngredienteRow from '$lib/components/ingredientes/IngredienteRow.svelte'
   import Tabla from '$lib/components/generales/tabla/Tabla.svelte'
   import { INGREDIENTES_MOCK } from '$lib/data/mocks/ingredientesMock'
   import { goto } from '$app/navigation'
-  
+
   import eye from '$lib/assets/eye.svg'
   import pencil from '$lib/assets/pencil-simple.svg'
   import trash from '$lib/assets/trash.svg'
 
+  const editar = (id) => { goto (`/editar-ingrediente/${id}`)}
 </script>
 
 {#snippet nombreColumnas()}
@@ -27,11 +29,17 @@
       {/snippet}
       {#snippet acciones()}
         <div class="iconos-acciones">
-          <img src={eye} alt="ojo" class="icono-ojo">
-          <a href={`./editar-ingrediente/${ingrediente.id}`}>
+          <!-- AGREGAR ACCION PARA EL ICONO BOTON EYE -->
+          <IconoBoton claseIcono="icono-ojo">
+            <img src={eye} alt="ojo">
+          </IconoBoton>
+          <IconoBoton onclick={() => editar(ingrediente.id)} >
             <img src={pencil} alt="lapiz">
-          </a>
-          <img src={trash} alt="tacho">
+          </IconoBoton>
+          <!-- AGREGAR ACCION PARA EL ICONO BOTON TRASH -->
+          <IconoBoton>
+            <img src={trash} alt="tacho">
+          </IconoBoton>
         </div>
       {/snippet}
     </IngredienteRow>
