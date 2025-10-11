f08c70355e0658892ecef92082c4945c650bd5fb
David Pazos <david_xen06@hotmail.com>
2025-10-11 00:54:55 -0300

    Agrego un test simple para probar el renderizado de la página en los tests

--- src/lib/tests/perfilLocal.test.ts
+++ src/lib/tests/perfilLocal.test.ts
@@ -1,7 +1,18 @@
+import '@testing-library/jest-dom'
 import { Local } from '$lib/models/local.svelte'
 import { describe, it, expect, beforeEach } from 'vitest'
+import { render, screen } from '@testing-library/svelte'
+import ProfileCard from '$lib/components/perfil-local/profile-card.svelte'
 
-describe('Local', () => {
+describe('ProfileCard', () => {
+  it('renderiza el título correctamente', () => {
+    render(ProfileCard, { props: { title: 'Perfil del Local' } })
+    const heading = screen.getByText('Perfil del Local')
+    expect(heading).toBeInTheDocument()
+  })
+})
+
+describe('Restaurar valores originales', () => {
   let local: Local
 
   beforeEach(() => {
@@ -16,20 +27,21 @@ describe('Local', () => {
     local.porcentajeAutor = 3
     local.metodosDePago = { QR: true, Efectivo: false, Transferencia: true }
 
-    // Guardamos copia original
+    // Copia original para después restaurarla
     local.copiaOriginal()
   })
 
   it('debería restaurar los valores originales luego de modificar los campos', () => {
-    // Hago algunos cambios, para después descartarlos y chequear que todo vuelva a como estaba antes
     local.nombreLocal = 'Starbucks'
     local.porcentajeApp = 99
     local.metodosDePago.QR = false
 
+    expect(local.nombreLocal).toBe('Starbucks')
+
     local.restaurarValores()
 
-    expect(local.nombreLocal).toBe('Taberna de Moe')
     expect(local.porcentajeApp).toBe(3)
     expect(local.metodosDePago.QR).toBe(true)
+    expect(local.nombreLocal).toBe('Taberna de Moe')
   })
 })
