6978668794979e4f5327c7f4e666c1834e13da3a
carlasolrocca <carlasolrocca@gmail.com>
2025-10-12 19:35:36 -0300

    Creo url /pedidos/{estado} para ruteo dinamico y agrego redireccion para que /pedidos siga siendo el home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-	import './(app)/pedidos-actuales.css'
-	import './general.css'
-	import Logo from '$lib/assets/logo.svg'
-	import Toast from '$lib/components/toasts/Toast.svelte'
+  import './general.css'
+  import Logo from '$lib/assets/logo.svg'
+  import Toast from '$lib/components/toasts/Toast.svelte'
 
-	let { children } = $props()
+  let { children } = $props()
 </script>
 
 <svelte:head>
-  <link href="https://fonts.googleapis.com/css2?family=Nata+Sans:wght@100..900&display=swap" rel="stylesheet">
-	<link rel="icon" href={Logo} />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nata+Sans:wght@100..900&display=swap"
+    rel="stylesheet"
+  />
+  <link rel="icon" href={Logo} />
 </svelte:head>
 <Toast />
 
