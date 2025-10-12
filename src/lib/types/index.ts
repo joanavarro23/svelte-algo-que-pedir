071a3a42c141bf8753a3d44edf4e5231da95aea0
carlasolrocca <carlasolrocca@gmail.com>
2025-10-12 20:51:27 -0300

    Export de estadoRutas.ts

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,4 @@
 export type { Pedido } from './pedido'
 export { MedioDePago, EstadoDelPedido } from './pedido'
 export type { Plato } from '../models/plato.svelte'
+export { rutasSegunEstado, estadoARuta } from './estadoRutas' 
