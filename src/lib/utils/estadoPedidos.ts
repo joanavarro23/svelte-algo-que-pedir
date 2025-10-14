a2c6d770099a453cb6785b3abe715b72f6eae830
carlasolrocca <carlasolrocca@gmail.com>
2025-10-14 08:50:46 -0300

    Saco posibilidad de estado en formato plural

--- src/lib/utils/estadoPedidos.ts
+++ src/lib/utils/estadoPedidos.ts
@@ -3,36 +3,22 @@
 
 import { EstadoDelPedido } from '$lib/types'
 
-//Si en el query params viene un "pendientes" o "PENDIENTES", lo acepta
+//Si en el query params viene un "pendiente" o "PENDIENTE", lo acepta
 export const aceptaEstadoMinMayus = (estado? : string | null) : EstadoDelPedido | null => {
   const estadoNormalizado = estado?.trim().toLowerCase()
 
   switch (estadoNormalizado) {
-  case 'pendientes':
+  case 'pendiente':
     return EstadoDelPedido.Pendiente
-  case 'preparados':
+  case 'preparado':
     return EstadoDelPedido.Preparado
-  case 'entregados':
+  case 'entregado':
     return EstadoDelPedido.Entregado
-  case 'cancelados':
+  case 'cancelado':
     return EstadoDelPedido.Cancelado
   default:
-    return null             //Si la ruta es /pedidos asi sin nada, no hace nada. Por eso admite la posibilidad de null.
+    return null             //Si la ruta es /pedidos asi sin nada, va a ir al error del +page.ts
   }
 }
 
-//Auxiliar para poder convertir a plural el estado para agregarlo a la URL. Lo usa el onclick del boton del +page.svelte
-//para poder concatenarlo como string en la url.
-export const estadoPlural = (estado: EstadoDelPedido): string => {
-  switch (estado) {
-    case EstadoDelPedido.Pendiente:
-      return 'pendientes'
-    case EstadoDelPedido.Preparado:
-      return 'preparados'
-    case EstadoDelPedido.Entregado:
-      return 'entregados'
-    case EstadoDelPedido.Cancelado:
-      return 'cancelados'
-  }
-}
 
