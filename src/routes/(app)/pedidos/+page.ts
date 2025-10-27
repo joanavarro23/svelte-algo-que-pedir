5b2f78258a82c9c0bd35671e22e8e858c74d8ca0
carlasolrocca <carlasolrocca@gmail.com>
2025-10-27 11:55:06 -0300

    Agrego en load que devuelva el valor de estado por la url

--- src/routes/(app)/pedidos/+page.ts
+++ src/routes/(app)/pedidos/+page.ts
@@ -1,14 +1,20 @@
 import { pedidoService } from '$lib/services/pedidoService'
 import { showError } from '$lib/utils/errorHandler'
+import { EstadoDelPedido } from '$lib/models/estadosPedido'
+import type { PageLoad } from './$types'
 
-export const load = async ( { depends } ) => {
+export const load : PageLoad = async ( { url, depends } ) => {
+  depends('pedidos:list')
   try{
-    depends('pedidos:list')
+    const paramsUrl = url.searchParams.get('estado')?.toUpperCase() as keyof typeof EstadoDelPedido | null
+    const estado = paramsUrl ? EstadoDelPedido[paramsUrl] : null
+
     const pedidos = await pedidoService.todosLosPedidos()
-    return { pedidos }
+    return { estado, pedidos }
+    
   } catch (error) {
     showError('Fallo la conexion al servidor', error)
-    return {pedidos: []}
+    return {estado: EstadoDelPedido, pedidos: []}
   }
 }
 
