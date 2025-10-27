f7560965ff457eb5b7397d84940dce456263dc09
carlasolrocca <carlasolrocca@gmail.com>
2025-10-26 23:37:24 -0300

    Utilizo el archivo para poner Enum del MedioDePago y array de {} para matchear valor del back con enum de Front

--- src/lib/models/metodosDePago.svelte.ts
+++ src/lib/models/metodosDePago.svelte.ts
@@ -1,1 +1,22 @@
 export type MetodoDePago = 'Efectivo' | 'QR' | 'Transferencia'
+
+//Enums del front de medios de pago
+export enum MedioDePago {
+  EFECTIVO = 'Efectivo',
+  QR = 'Qr',
+  TARJETA = 'Tarjeta de crédito'
+}
+
+//Mapeo de los valores de Enum del BACK con el enum MedioDePago del FRONT
+const medioPagoTitlecase : {labelBack : string, medio : MedioDePago}[] = [
+  {labelBack: 'EFECTIVO', medio: MedioDePago.EFECTIVO},
+  {labelBack: 'QR', medio: MedioDePago.QR},
+  {labelBack: 'TARJETA', medio: MedioDePago.TARJETA},
+]
+
+//Funcion que asocia los valores del Back con el enum del Front y retorna el match correspondiente
+export function medioPagoDesdeBack(labelBack: string) : MedioDePago {
+  const key = labelBack?.trim().toUpperCase()
+  const match = medioPagoTitlecase.find(m => m.labelBack === key)
+  return match ? match.medio : MedioDePago.EFECTIVO
+}
--- src/lib/utils/medioPagoIcono.ts
+++ src/lib/utils/medioPagoIcono.ts
@@ -1,14 +1,15 @@
-import { MedioDePago } from '$lib/types'
+import { MedioDePago } from '$lib/models/metodosDePago.svelte'
 
 import tarjetaIcono from '$lib/assets/tarjeta-credito.svg'
 import efectivoIcono from '$lib/assets/efectivo.svg'
 import QRIcono from '$lib/assets/codigo-qr.svg'
 
 export const mapaIconoPago = {
-  [MedioDePago.Efectivo] : efectivoIcono,
+  [MedioDePago.EFECTIVO] : efectivoIcono,
   [MedioDePago.QR] : QRIcono,
-  [MedioDePago.Tarjeta] : tarjetaIcono
+  [MedioDePago.TARJETA] : tarjetaIcono
 } 
 
-
-
+export function iconoMedioPago(medio: MedioDePago) : string {
+  return mapaIconoPago[medio]
+}
