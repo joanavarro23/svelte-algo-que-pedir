494fcbb5ca351ba427f95e1c861d4d96ba5ab6b1
carlasolrocca <carlasolrocca@gmail.com>
2025-10-27 13:01:55 -0300

    Saco export innecesario

--- src/lib/utils/medioPagoIcono.ts
+++ src/lib/utils/medioPagoIcono.ts
@@ -4,7 +4,7 @@ import tarjetaIcono from '$lib/assets/tarjeta-credito.svg'
 import efectivoIcono from '$lib/assets/efectivo.svg'
 import QRIcono from '$lib/assets/codigo-qr.svg'
 
-export const mapaIconoPago = {
+const mapaIconoPago = {
   [MedioDePago.EFECTIVO] : efectivoIcono,
   [MedioDePago.QR] : QRIcono,
   [MedioDePago.TARJETA] : tarjetaIcono
