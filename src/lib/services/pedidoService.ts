5f4a82520f0dba1e6101753420e81dad452a1c7e
carlasolrocca <carlasolrocca@gmail.com>
2025-10-26 23:37:39 -0300

    Armo el Pedido Service con el pedidosByEstado

--- src/lib/services/pedidoService.ts
+++ src/lib/services/pedidoService.ts
@@ -1,6 +1,19 @@
-import { Pedido, type PedidoJSON } from '$lib/models/pedido.svelte.ts'
+import { Pedido, type PedidoJSON } from '$lib/models/pedido.svelte'
 import { getAxiosData } from '$lib/services/common'
 import { REST_SERVER_URL } from '$lib/services/configuration'
 import axios from 'axios'
 
 
+class PedidoService {
+  async todosLosPedidos() {
+    const queryPedidos = () => axios.get<PedidoJSON[]>(`${REST_SERVER_URL}/pedidos`)
+    return (await getAxiosData(queryPedidos)).map(Pedido.fromJSON)
+  }
+
+  async pedidoByEstado(estado : string){
+    const queryPedidosEstado = () => axios.get<PedidoJSON[]>(`${REST_SERVER_URL}/pedidos?estado=${estado}`)
+    return (await getAxiosData(queryPedidosEstado)).map(Pedido.fromJSON)
+  }
+}
+
+export const pedidoService = new PedidoService()
